d2db0fcdd77cdd491cc27212f4270f68c3dd87e5
shytuleva24 <shytuleva@gmail.com>
2025-09-27 11:02:06 +0200

    Add account info component with details and attention items

--- src/app/features/account/components/account-info/account-info.ts
+++ src/app/features/account/components/account-info/account-info.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { SvgIcon } from '@shared/ui/svg-icon/svg-icon';
+
 @Component({
   selector: 'app-account-info',
-  imports: [],
+  imports: [SvgIcon],
   templateUrl: './account-info.html',
   styleUrl: './account-info.scss',
 })
